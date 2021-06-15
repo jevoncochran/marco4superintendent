27f8af0009434fd00bcc95940db03a2cd6a7eaee
Jevon Cochran <jevon.cochran@gmail.com>
2021-06-15 16:53:05 -0400

    removed unnecessary periods from platform

--- website/data/platform.js
+++ website/data/platform.js
@@ -8,11 +8,11 @@ export const platform = [
     text: "Free Health Insurance. A $70,000 Teacher Minimum Annual Salary. A $25 dollar an hour minimum wage for Classified employees. We’re going to cut from the top, starting with our office. Our campaign believes that our education system favors a business model type governance, where district superintendents are paid like CEOs of major companies. We are going to cut from the top. We believe that Billionaires shouldn’t exist, we believe that most Californians believe this too. We are going to fight for a public education system that is for the public.",
   },
   {
-    point: "End The School-To-Prison-Pipeline.",
+    point: "End The School-To-Prison-Pipeline",
     text: "We believe in the Abolitionist Movement. The only way to stop having schools be vectors to prison is by believing we can get to a state, in fact, a world, without prisons. Consequently, we also believe that schools should not have Police. We propose the creation of a one-year committee composed of Activists, Formerly Incarcerated People, Engaged Critical Scholars, and Families to create a comprehensive and critical proposal to get every Student Resource Officer (SRO) out of our schools; additionally, the plan must propose what our students and employees would require to be safe in our schools.",
   },
   {
-    point: "End Military Recruitment in Our Schools.",
+    point: "End Military Recruitment in Our Schools",
     text: "We must fight for the safety and freedom of our kids on a state level and national level. The California Department of Education will lead a national effort to end Section 9528 of George Bush’s 2001 No Child Left Behind Bill – a devasting education bill that served as a massive transfer of wealth from every day schools and students to the already wealthy class.",
   },
   {
@@ -22,24 +22,24 @@ export const platform = [
   },
   {
     point:
-      "Full Rights for People with dis/Abilities in School and out of School.",
+      "Full Rights for People with dis/Abilities in School and out of School",
     text: "Our schools need to fully value all our students. The Department of Education will aim to increase the recruitment and retention of Special Education Teachers and will value the expertise they bring into the classroom. We will propose a whole set of systemic reforms that will dramatically change the profession as we know it, to help Special Education teachers do what they do best, teach. Students will receive the appropriate resources and accommodations to meet their IEP Goals and Transition Goals. We will provide our community colleges, universities, and trade schools with the funds needed to appropriately meet the needs of all our students with an IEP. Students with a dis/Ability have the right to a dignified life. A different world for them is possible.",
   },
   {
-    point: "Dual-Language for All. Decolonize Language-Arts.",
+    point: "Dual-Language for All. Decolonize Language-Arts",
     text: "We will fully fund Proposition 58. Every student in California should have the right to learn more than one language throughout the course of their educational career. The Department of Education will aim to amend the California Constitution to include an amendment that reads: “The state recognizes the distinct and unique cultural heritage of Californian Indigenous communities and nations, and is committed in its educational goals to the preservation of their cultural integrity.”",
   },
   {
-    point: "Public Universities, Colleges, and Technical Schools Must be Free.",
+    point: "Public Universities, Colleges, and Technical Schools Must be Free",
     text: "This isn’t a pipedream. We have done this before, when we weren’t the 5th largest economy on Earth, we can do it again. The State of California can and should invest in its future and prosperity by making sure that every Californian that wants to attend a 4-year university or Trade/Technical School can. California has the most diverse and skillful population in the nation. It’s time we invest in their future, which is our collective future.",
   },
   {
-    point: "End Standardized Testing.",
+    point: "End Standardized Testing",
     text: "From the UC system to the Ivy League, from university researchers to K-12 Teachers, most people that work in education realize that Standardized Testing is an outdated form of assessing a student or group of student’s academic performance. California must lead the country in the implementation of more nuanced and collective forms of assessment that take into consideration the knowledge and experiences of the whole child.",
   },
   {
     point:
-      "Through the Power of The People We Will Build A Conscientious Education System.",
+      "Through the Power of The People We Will Build A Conscientious Education System",
     text: "Californian students deserve an education system that not only fights for their academic and scholarly rights, it deserves one that fights for their human rights – for “a world where many worlds can exist” – as the Zapatistas state.",
   },
 ];
