ff12a3cb65b07fb8ae096d86073d17d1937d324c
Jevon Cochran <jevon.cochran@gmail.com>
2022-01-08 00:14:40 -0500

    twitter and donate popover

--- website/pages/index.js
+++ website/pages/index.js
@@ -1,12 +1,31 @@
+import { useEffect, useContext } from "react";
 import Layout from "../components/Layout";
 import Intro from "../components/Home/Intro";
 import JoinThisMovement from "../components/Home/JoinThisMovement";
+import DonatePopOver from "../components/Home/DonatePopOver";
+import { setCookies } from "cookies-next";
+import { SiteContext } from "../context/siteContext";
 
-export default function Home() {
+export default function Home({ pageVisited }) {
+  const { hideDonatePopover } = useContext(SiteContext);
+
+  useEffect(() => {
+    if (!pageVisited) {
+      setCookies("pageVisited", true);
+    }
+  }, []);
+
   return (
     <Layout>
+      {!pageVisited && !hideDonatePopover && <DonatePopOver />}
       <Intro />
       <JoinThisMovement />
     </Layout>
   );
 }
+
+export const getServerSideProps = async (ctx) => {
+  const { pageVisited } = ctx.req.cookies;
+
+  return { props: { pageVisited: pageVisited ?? null } };
+};
